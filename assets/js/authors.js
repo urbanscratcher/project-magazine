43b17f513cddac36b285de511a196b22a30ff986
Joun <urbanscratcher@gmail.com>
2023-08-20 19:54:19 +0900

    avatar data binding

--- assets/js/authors.js
+++ assets/js/authors.js
@@ -6,9 +6,14 @@
 fetch("/data/authors/data.json")
   .then((res) => res.json())
   .then((data) => {
-    //load external js
-    const insightsScript = document.createElement("script");
-    insightsScript.src = "/assets/js/insights.js";
-    insightsScript.onload();
+    const authors = [...data.authors]
+      .sort((a, b) => b.id - a.id)
+      .slice(0, 4)
+      .map((el) => {
+        el.topicsOneline = el.topics.join("&nbsp;·&nbsp;");
+        return el;
+      });
+
+    insertAfterTemplate("featuredAuthorsTemplate", { data: authors });
   })
   .catch((err) => console.error("error: ", err));
