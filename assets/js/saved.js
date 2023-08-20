5e12e5fe2f119e0e229dfd538591acbacff30d05
Joun <urbanscratcher@gmail.com>
2023-08-20 18:39:09 +0900

    saved for later data binding done

---
+++ assets/js/saved.js
@@ -0,0 +1,30 @@
+fetch("/data/insights/data-saved.json")
+  .then((res) => res.json())
+  .then((data) => {
+    fetch("/data/insights/data.json")
+      .then((res) => res.json())
+      .then((insightsData) => {
+        fetch("/data/authors/data.json")
+          .then((res) => res.json())
+          .then((authorsData) => {
+            const list = data.insightsSaved
+              .sort((a, b) => new Date(b.savedAt) - new Date(a.savedAt))
+              .slice(0, 4);
+            const savedList = list.map((el) => {
+              const insight = insightsData.insights.find(
+                (insight) => insight.id === el.id
+              );
+
+              insight.author = authorsData.authors.find(
+                (author) => author.id === insight.id
+              );
+
+              const mappedEl = { ...el, ...insight };
+
+              return mappedEl;
+            });
+
+            insertAfterTemplate("savedTemplate", { data: savedList });
+          });
+      });
+  });
