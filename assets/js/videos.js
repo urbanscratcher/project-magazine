b1498f6551d036c6a11ad3632a2f9a77a10a62f2
Joun <urbanscratcher@gmail.com>
2023-08-21 11:48:26 +0900

    video thumbnail dummy

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -8,8 +8,14 @@ fetch("/index.html")
     fetch("/data/videos/data.json")
       .then((res) => res.json())
       .then((data) => {
-        const list = data.videos.slice(1, 5);
-        insertAfterTemplate("videoCoverTemplate", data.videos[0]);
+        const lowerCasedData = data.videos.map((el) => {
+          el.title = el.title.toLowerCase();
+          return el;
+        });
+
+        const list = lowerCasedData.slice(1, 5);
+
+        insertAfterTemplate("videoCoverTemplate", lowerCasedData[0]);
         insertAfterTemplate("videoListTemplate", { data: list });
       })
       .catch((err) => console.error("error: ", err));
